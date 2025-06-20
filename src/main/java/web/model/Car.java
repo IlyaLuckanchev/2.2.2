7c0d59730613f122760dad0471ac2c609f96c695
Ilya Lukanchev <bkmz432111@yandex.ru>
2025-06-20 20:57:24 +0400

    7.1

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -5,8 +5,6 @@ public class Car {
     private String color;
     private int price;
 
-    public Car() {
-    }
 
     public Car(String model, String color, int price) {
         this.model = model;
@@ -14,31 +12,18 @@ public class Car {
         this.price = price;
     }
 
-
-
     public String getModel() {
         return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
     }
 
     public String getColor() {
         return color;
     }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
-
     public int getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
-    }
     @Override
     public String toString() {
         return "Car{" +
